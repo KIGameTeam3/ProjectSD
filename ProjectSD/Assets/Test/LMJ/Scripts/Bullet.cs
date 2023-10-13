ffe684a9ce3a39d4d9ce3eade60f78cb7137e12c
MiJeong <dlalwjd3323@naver.com>
2023-10-13 15:03:07 +0900

    이미정 : [Add] Bullet Prefab, Bullet script 생성

---
+++ ProjectSD/Assets/Test/LMJ/Scripts/Bullet.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 8f;  // Åº¾Ë ¼Ó·Â
+    private Rigidbody bulletRigidbody = default;    // Åº¾Ë Rigidbody ÄÄÆ÷³ÍÆ®
+}
