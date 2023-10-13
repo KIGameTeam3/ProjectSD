437602a295ef123534cca6e2ac40ffb3162fe7ae
tkdtkddmlehdanf97@gmail.com <tkdtkddmlehdanf97@gmail.com>
2023-10-13 18:21:32 +0900

    김형준 [Add]  Scripts 폴더에 스크립트 3개 생성, 플레이어,시작,종료, 상점 UI Canvas 생성 및 각각의 하위에 이미지 오브젝트 생성 스크립트 : GetUnit - Grid Layer 오브젝트 하위에 있으며  버프가 생기면 불러와서 생성하고 시간이 끝나면 사라지게 하는 스크립트 , KHJUIManager - 일단 UI 들 관리하고자 생성- 나중에 gameManager 에서 불러와서 사용할 수 있게끔 구현 - 여기 업데이트에서 Time 함수가 실행될 예정 , PlayerUIControl - (삭제할 수도 있습니다)  플레이어 관련 ui들을 관리하고자 생성 캔버스 : 단순하게 상점은 월드 공간에 존재하게 설정 , 플레이어 UI는 해상도 1920 x 1080 설정 이미지 : 코인, 체력, 버프 대체할 이미지 생성

---
+++ ProjectSD/Assets/Test/KHJ/Scripts/PlayerUiControl.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUiControl : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
