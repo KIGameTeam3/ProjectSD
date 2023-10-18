a07705add7d22eadfff8824d589b0c28f287a932
tkdtkddmlehdanf97@gmail.com <tkdtkddmlehdanf97@gmail.com>
2023-10-18 11:55:29 +0900

    김형준 : [Add] 리소스 폴더 하위에 버튼 오브젝트 프리팹을 가져와서 변수에 등록한 뒤  지정한 수만큼 생성해서 캔버스 하위에 생성하는 스크립트 생성

--- ProjectSD/Assets/Test/KHJ/Scripts/GetUnitBtn.cs
+++ ProjectSD/Assets/Test/KHJ/Scripts/GetUnitBtn.cs
@@ -6,14 +6,33 @@ public class GetUnitBtn : MonoBehaviour
 {
     [SerializeField] private GameObject btnPrefab;
     // Start is called before the first frame update
+    void Awake()
+    {
+        btnPrefab = Resources.Load<GameObject>("UnitBtnPrefabs/UnitBtnModel");    
+    }
+    private void OnEnable()
+    {
+        Debug.Log("버튼 생성기 켜지나요?");
+        //CreateUnitBtn(4);
+    }
     void Start()
     {
-        
+        CreateUnitBtn(4);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+   
+    //생성해야 하는 수만큼 버튼 프리팹 생성
+    //그런데 얼만큼 생성해야하는지 어떻게 알지?
+    public void CreateUnitBtn(int unitCount)
+    {
+        for(int i = 0; i < unitCount; i++ )
+        {
+            Instantiate(btnPrefab,this.transform);
+        }
     }
 }
