301a160dbe0bc62a61826761855dcc596b1e370a
tkdtkddmlehdanf97@gmail.com <tkdtkddmlehdanf97@gmail.com>
2023-10-19 13:26:45 +0900

    김형준 : [Feat] 왼쪽 컨트롤러 hand 상태에서 y버튼 누르면 상점창 열리는 기능 추가

--- ProjectSD/Assets/Test/KHJ/Scripts/Aim.cs
+++ ProjectSD/Assets/Test/KHJ/Scripts/Aim.cs
@@ -56,58 +56,16 @@ public class Aim : MonoBehaviour
                 DetectR();
             }
         } 
-
         //타워를 설치한 이후
         else
         {
-            //left
-            Vector3 startPos = ARAVRInput.LHandPosition;
-            Vector3 pos = ARAVRInput.LHandDirection;
-            pos.y = 0;
-            Vector3 endPos = (startPos + (pos.normalized * lrMaxDistance));
-            endPos.y = 0;
-
-            // 왼쪽 컨트롤러 기준으로 Ray를 만든다.
-            Ray ray = new Ray(startPos, ARAVRInput.LHandDirection);
-            RaycastHit hitInfo;
-
-            // 충돌이 있다면?
-            if (Physics.Raycast(ray, out hitInfo, lrMaxDistance, GlobalFunction.GetLayerMask("Floor")))
-            {
-                endPos = hitInfo.point;
-            }
-            else
-            {
-                //예외처리
-                //1. 아무것도 감지 못했을때 그 최대치의 바닥이 floor가 아닐때
-                //2. Vector3.up이나 Vector3.down일때 위치
-
-                lrMaxDistance = 30f;
-                if (pos.magnitude < 0)
-                {
-                    //endPos = 
-                }
-               
-            }
-
-            preview.transform.position = endPos;
-
-            // Ray가 부딪힌 지점에 라인 그리기
-            lineRenderer.SetPosition(0, startPos);
-            lineRenderer.SetPosition(1, endPos);
-
-            //TODO 설치하는 함수 실행
-            if (ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger, ARAVRInput.Controller.LTouch) && btn != null && preview.installable)
-            {
-                btn.SetInUnit(endPos);
-            }
+            ShowTowerCheck();
         }
-    
     } //Update()
 
     void MouseDetect()
     {
-        Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
 
         // 충돌이 있다면?
@@ -122,7 +80,7 @@ public class Aim : MonoBehaviour
                 Debug.Log(Input.GetAxis("Fire1"));
                 UIHitCollider hitObject = hitInfo.transform.GetComponent<UIHitCollider>();
 
-                if (Input.GetAxisRaw("Fire1")==1)
+                if (Input.GetAxisRaw("Fire1") == 1)
                 {
                     Debug.Log("파이어1");
                     // 컨트롤러의 진동 재생
@@ -130,7 +88,7 @@ public class Aim : MonoBehaviour
                     hitObject?.HitUI();
                 }
             }
-            else if(hitInfo.collider.tag == "PlayerUi")
+            else if (hitInfo.collider.tag == "PlayerUi")
             {
                 UIHitCollider hitObject = hitInfo.transform.GetComponent<UIHitCollider>();
                 if (Input.GetButtonDown("Fire1"))
@@ -142,7 +100,7 @@ public class Aim : MonoBehaviour
             }
         }
     }
-    void DetectL()
+    public void DetectL()
     {
         Vector3 startPos = ARAVRInput.LHandPosition;
         Vector3 endPos = startPos + ARAVRInput.LHandDirection * lrMaxDistance;
@@ -170,24 +128,19 @@ public class Aim : MonoBehaviour
                     btn = hitInfo.collider.gameObject.GetComponent<BuyUnit>();
                     
                 }
-               /* else if (hitInfo.collider.tag == "PlayerUi")
-                {
-                    if (Input.GetButtonDown("Fire1"))
-                    {
-                        // 컨트롤러의 진동 재생
-                        //ARAVRInput.PlayVibration(ARAVRInput.Controller.RTouch);
-                        hitObject?.HitUI();
-                    }
-                }*/
             } 
-
+        }
+        //손만 있는 상태에서 왼쪽 컨트롤러 Y 버튼 누르면 상점창 출력 / 죽었을때는  출력 못하게끔
+        else if (ARAVRInput.GetDown(ARAVRInput.Button.Two, ARAVRInput.Controller.LTouch))
+        {
+            OpenShopInPlay();
         }
         // Ray가 부딪힌 지점에 라인 그리기
         lineRenderer.SetPosition(0, startPos);
         lineRenderer.SetPosition(1, endPos);
 
     }
-    void DetectR()
+    public void DetectR()
     {
 
         Vector3 startPos = ARAVRInput.RHandPosition;
@@ -217,15 +170,6 @@ public class Aim : MonoBehaviour
                     btn = hitInfo.collider.gameObject.GetComponent<BuyUnit>();
 
                 }
-                /*else if (hitInfo.collider.tag == "PlayerUi")
-                {
-                    if (Input.GetButtonDown("Fire1"))
-                    {
-                        // 컨트롤러의 진동 재생
-                        //ARAVRInput.PlayVibration(ARAVRInput.Controller.RTouch);
-                        hitObject?.HitUI();
-                    }
-                }*/
             }
         }
         // Ray가 부딪힌 지점에 라인 그리기
@@ -252,4 +196,55 @@ public class Aim : MonoBehaviour
         lineRenderer.SetPosition(0, startPos);
         lineRenderer.SetPosition(1, endPos);
     }       // else : 오른쪽 핸드 기준으로 레이저 포인터 만들기
+    public void ShowTowerCheck()
+    {
+        //left
+        Vector3 startPos = ARAVRInput.LHandPosition;
+        Vector3 pos = ARAVRInput.LHandDirection;
+        pos.y = 0;
+        Vector3 endPos = (startPos + (pos.normalized * lrMaxDistance));
+        endPos.y = 0;
+
+        // 왼쪽 컨트롤러 기준으로 Ray를 만든다.
+        Ray ray = new Ray(startPos, ARAVRInput.LHandDirection);
+        RaycastHit hitInfo;
+
+        // 충돌이 있다면?
+        if (Physics.Raycast(ray, out hitInfo, lrMaxDistance, GlobalFunction.GetLayerMask("Floor")))
+        {
+            endPos = hitInfo.point;
+        }
+        else
+        {
+            //예외처리
+            //1. 아무것도 감지 못했을때 그 최대치의 바닥이 floor가 아닐때
+            //2. Vector3.up이나 Vector3.down일때 위치
+
+            lrMaxDistance = 30f;
+            if (pos.magnitude < 0)
+            {
+                //endPos = 
+            }
+
+        }
+
+        preview.transform.position = endPos;
+
+        // Ray가 부딪힌 지점에 라인 그리기
+        lineRenderer.SetPosition(0, startPos);
+        lineRenderer.SetPosition(1, endPos);
+
+        //TODO 설치하는 함수 실행
+        if (ARAVRInput.GetDown(ARAVRInput.Button.IndexTrigger, ARAVRInput.Controller.LTouch) && btn != null && preview.installable)
+        {
+            btn.SetInUnit(endPos);
+        }
+    }
+    public void OpenShopInPlay()
+    {
+        if(GameManager.Instance.playerState != PlayerState.DEAD )
+        {
+            KHJUIManager.Instance.OpenShop();
+        }
+    }
 }
